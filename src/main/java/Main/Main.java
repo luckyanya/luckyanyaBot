85b31db9f2e28bf95636f538b4b198924ac1b244
Anna <anya.lakeeva1@mail.ru>
2019-10-10 19:00:36 +0300

    ошибка

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,85 @@
+package Main;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main extends TelegramLongPollingBot {
+
+    public static final String USERNAME = "luckyanyaBot";
+    public static final String TOKEN = "945476768:AAH_vWOWuJlBssboE258Smc7x6a-6boo2cc";
+
+    public Main(DefaultBotOptions botOptions) {
+        super(botOptions);
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return USERNAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return TOKEN;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        SendMessage sendMessage = new SendMessage(message.getChatId(), message.getText());
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+            botOptions.setProxyHost("177.185.156.241");
+            botOptions.setProxyPort(8080);
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+            telegramBotsApi.registerBot(new Main(botOptions));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
+/*public class Mainn extends TelegramLongPollingBot {
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] arguments) {
+        String userName = chat.getUserName();
+        if (userName == null || userName.isEmpty()) {
+            userName = user.getFirstName() + "" + user.getLastName();
+        }
+        StringBuilder messageTextBuilder = new StringBuilder("Hello").append(userName);
+        if (arguments != null && arguments.length > 0) {
+            messageTextBuilder.append("\n");
+            messageTextBuilder.append("Thank you so much for your kind words:\n");
+            messageTextBuilder.append(String.join("", arguments));
+        }
+
+        SendMessage answer = new SendMessage();
+        answer.setChatId(chat.getId().toString());
+        answer.setText(messageTextBuilder.toString());
+        try {
+            absSender.execute(answer);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+
